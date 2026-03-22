96cc3cc1ef606363daa78988cfed40b191ebaebb
V S Satish Reddy Mule <mule@Vs-MacBook-Air.attlocal.net>
2026-03-22 10:05:15 -0500

    Switched from SQLite to PostgreSQL

--- app/db/database.py
+++ app/db/database.py
@@ -1,10 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./fraud.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
